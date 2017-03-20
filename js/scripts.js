fede04627664dd895447ee5c5f25c775ef40ea7f
Benjamin Petrosky and Ilene Gorski <ilene@>
2017-03-20 13:51:31 -0700

    Provide conversions for some cooking measurements.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// var gallons=parseInt(prompt("Enter the amount in gallons"));
+// liters=gallons*3.7854;
+// alert(liters);
+//
+// var liters=parseInt(prompt("Enter the amount in liters"));
+// gallons=liters *.2642;
+// alert(gallons);
+//
+// var ounces=parseInt(prompt("Enter the amount in Ounces"));
+// // Quart=ounces / 32;
+// // alert(Quart);
+//
+// var tablespoons=parseInt(prompt("Enter the amount in tablespoons"));
+// gills=tablespoons / 8;
+// tablespoons=tablespoons.toString();
+// gills=gills.toString();
+// alert(tablespoons + " tablespoons is " + gills + " gills.");
+
+var ounces=parseInt(prompt("Enter the amount in ounces"));
+gill=ounces / 4;
+alert(gill + " gills is " + ounces + " ounces.")
